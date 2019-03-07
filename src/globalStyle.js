74d520702d84fb45c6e5e1fbb713cdcc983b512b
Andy Lee <andy5090@naver.com>
2019-03-07 16:15:32 +0900

    Initial commit and transplant from previous doc-glue project

---
+++ src/globalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color : #d2dae2;
+    padding-top: 10px;
+    margin-left: 60px;
+    margin-right: 60px;
+  }
+`;
+
+export default GlobalStyle;
